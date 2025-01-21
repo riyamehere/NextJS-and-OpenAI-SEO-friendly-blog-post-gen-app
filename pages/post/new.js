03e51c736e838cd9304d906e2e294643134e649e
Riya Mehere <riyamehere@gmail.com>
2025-01-21 21:09:36 +0530

    feat: Setting up files to work with GPT

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,8 +1,19 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import AppLayout from "../../components/AppLayout/AppLayout";
 export default function NewPost(props) {
+
+  const handleClick = async() => {
+    const response =  await fetch(`/api/generatePost`,{
+      method:'POST'
+    })
+    const json = await response.json();
+    console.log(json)
+  }
     return (
-    <div>This is new post</div>
+    <><div>This is new post</div>
+    <button className="btn" onClick={handleClick}>
+      Generate
+      </button></>
     );
   }
 
